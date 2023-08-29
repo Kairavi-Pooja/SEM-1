01a4d1dd295aa1bb771fb930c91150f108a97946
Kairavi-Pooja <141142674+Kairavi-Pooja@users.noreply.github.com>
2023-08-29 17:40:23 +0530

    Basic C Programming
    Programs:
    1. Print area of triangle
    2. Print simple interest

---
+++ triangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+             int base;
+             scanf("%d",&base);
+             int height;
+             scanf("%d",&height);
+             float area;
+             area = 0.5*(float)base*height;
+             printf("%0.1f",area);
+            }
