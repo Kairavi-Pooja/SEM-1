081bfcc6227467fc638ed11ae83e1829cd3f46f1
Kairavi-Pooja <141142674+Kairavi-Pooja@users.noreply.github.com>
2023-08-29 17:43:26 +0530

    Delete sum.c

--- sum.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main() {
-             int a,b;
-             printf("Enter an integer");
-             scanf("%d",a);
-             printf("Enter second integer");
-             scanf("%d",b);
-             printf("Sum is %d",a+b);
-             }
