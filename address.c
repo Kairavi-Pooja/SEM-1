e2b5910c5a3c345659f3225ad82dbdfbce7941a7
Kairavi-Pooja <141142674+Kairavi-Pooja@users.noreply.github.com>
2023-08-29 17:43:54 +0530

    Delete address.c

--- address.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-void main() {
-             printf("Address: 207/8, Street no.4");
-             printf("Nandnanvan park, Jamnagar");
-             printf("361005");
-            }
