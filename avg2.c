aa9957c01721a74e719d0354c194e7527ba032e6
Kairavi-Pooja <141142674+Kairavi-Pooja@users.noreply.github.com>
2023-08-29 17:44:51 +0530

    Delete avg2.c

--- avg2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main() {
-             int a=10;
-             int b=20;
-             int c=25;
-             float d;
-             d=((a+b+c)/(float)3);
-             printf("Average is %f",&d); 
-}
