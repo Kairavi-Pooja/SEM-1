0661655b415bc7c0273a224b7d7e47d99d845bf2
Kairavi-Pooja <141142674+Kairavi-Pooja@users.noreply.github.com>
2023-08-29 17:44:25 +0530

    Delete area.c

--- area.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main () { 
-              float area;
-              int r;
-              scanf("%d",&r);
-              area = 3.14*r*r;
-              printf("Area of circle is %0.2f",area);
-}
